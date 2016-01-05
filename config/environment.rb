f011e861627e292cd6883bf82315d9117a9b14d5
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-01-05 12:18:28 +0100

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+TunisianDictionary2016::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TunisianDictionary2016::Application.config.secret_token = '528231b1c9efcf010ce961f22c281ca9c39b0bfd812e754f61c7ae3514fb550cce392acc6146698b1d87e99dea2f782dafeeebd7b45892fcab8b264b545e4165'
